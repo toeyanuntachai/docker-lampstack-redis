566f440b16a3eaf31e8b7484bff42e1aa25b0858
Toey <toey.anuntachai@gmail.com>
2017-01-31 12:05:36 +0700

    Remove db

--- mysql/start_script.sh
+++ mysql/start_script.sh
@@ -3,8 +3,5 @@
 
 sleep 10;
 
-mysql --host=localhost --port=3306 --user=root --password=password -e "CREATE DATABASE test"
-mysql --host=localhost --port=3306 --user=root --password=password siriraj < /test.sql
-
 mysql --host=localhost --port=3306 --user=root --password=password -e "update mysql.user set Host = '%'"
 mysql --host=localhost --port=3306 --user=root --password=password -e "FLUSH PRIVILEGES"
